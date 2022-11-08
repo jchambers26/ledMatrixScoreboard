cc19d449d4927cd18a3ea114b74faf515cf767b2
jchambers26 <chambersjacobj@gmail.com>
2022-11-07 22:31:29 -0700

    Integrate EPL JSON data with text rendering

--- text.py
+++ text.py
@@ -1,5 +1,6 @@
 from rgbmatrix import graphics, RGBMatrix, RGBMatrixOptions
 import time
+import json
 from enum import Enum
 
 class Standings:
@@ -14,56 +15,105 @@ class Standings:
         self.path = "/home/pi/Documents/Scoreboard/"
         self.font_path = "/home/pi/Documents/rpi-rgb-led-matrix/fonts/"
 
+        self.colors = self.plColors()
 
-    def renderStandings(self, printer=False):
 
-        State = Enum('State', ['FIRST', 'SECOND', 'THIRD'])
+    def renderEPLStandings(self, printer=False):
 
-        state = State.SECOND
+        with open(self.path + 'eplStandings.json', 'r') as file:
+            standings = json.load(file)
+        
         matrix = RGBMatrix(options=self.options)
-
         canvas = matrix.CreateFrameCanvas()
 
-        font = graphics.Font()
-        font.LoadFont(self.font_path + "7x13.bdf")
-        textColor = graphics.Color(255, 0, 0)
-        pos1 = 0
-        pos2 = canvas.width
-        pos3 = canvas.width
+        State = Enum('State', ['FIRST', 'SECOND'])
 
-        font2 = graphics.Font()
-        font2.LoadFont(self.font_path + "5x8.bdf")
+        state = State.FIRST
 
-        while True:
-            canvas.Clear()
-            len1 = graphics.DrawText(canvas, font2, pos1, 6, graphics.Color(255, 255, 255), "EPL Standings")
-            graphics.DrawLine(canvas, 0, 7, 64, 7, graphics.Color(255, 255, 255))
-            yPos = 18
-            len2 = graphics.DrawText(canvas, font, pos2, yPos, textColor, "5. Manchester United")
-            yPos += 10
-            len3 = graphics.DrawText(canvas, font, pos3, yPos, graphics.Color(255, 255, 255), "23 Points 7-2-4")
+        teamFont = graphics.Font()
+        teamFont.LoadFont(self.font_path + "7x13.bdf")
 
-            if (pos2 != canvas.width - len2 - 1 and state == State.SECOND):
-                pos2 -= 1
-            elif (pos3 != canvas.width - len3 - 1 and state == State.THIRD):
-                pos3 -= 1
-        
-            if (pos2 == canvas.width - len2 - 1 and state == state.SECOND):
-                state = State.THIRD
-                pos2 = 0
-                time.sleep(0.5)
-                pos3 = canvas.width
-                continue
-            elif (pos3 == canvas.width - len3 - 1 and state == state.THIRD):
-                state = State.SECOND
-                pos3 = 0
-                time.sleep(0.5)
-                pos2 = canvas.width
-                continue
+        titleFont = graphics.Font()
+        titleFont.LoadFont(self.font_path + "5x8.bdf")
 
-            time.sleep(0.05)
-            canvas = matrix.SwapOnVSync(canvas)
+        # Loop over first to 20th place
+        for place in range(1, 21):
+
+            pos1 = canvas.width
+            pos2 = canvas.width
+
+            while True:
+                canvas.Clear()
+                # Draw the "EPL Standings" header with a line underneath
+                len1 = graphics.DrawText(canvas, titleFont, 0, 6, graphics.Color(255, 255, 255), "EPL Standings")
+                graphics.DrawLine(canvas, 0, 7, 64, 7, graphics.Color(255, 255, 255))
+
+                team = standings[str(place)]['team']
+                points = standings[str(place)]['points']
+                won = standings[str(place)]['won']
+                draw = standings[str(place)]['draw']
+                lost = standings[str(place)]['lost']
+
+                teamString = f"{place}. {team}"
+                dataString = f"{points} points {won}-{draw}-{lost}"
+
+                color = self.colors[team]
+
+                # Draw the team name with their table position
+                len2 = graphics.DrawText(canvas, teamFont, pos1, 18, color, teamString)
+                
+                # Draw the point total and record for the team
+                len3 = graphics.DrawText(canvas, teamFont, pos2, 28, color, dataString)
+
+                if (pos1 != canvas.width - len2 - 1 and state == State.FIRST):
+                    pos1 -= 1
+                elif (pos2 != canvas.width - len3 - 1 and state == State.SECOND):
+                    pos2 -= 1
+            
+                if (pos1 == canvas.width - len2 - 1 and state == state.FIRST):
+                    state = State.SECOND
+                    pos1 = 0
+                    time.sleep(0.5)
+                    pos2 = canvas.width
+                    continue
+                elif (pos2 == canvas.width - len3 - 1 and state == state.SECOND):
+                    state = State.FIRST
+                    pos2 = 0
+                    time.sleep(0.5)
+                    pos1 = canvas.width
+                    break
+
+                time.sleep(0.05)
+                canvas = matrix.SwapOnVSync(canvas)
+
+    
+    def plColors(self):
+        colors = {}
+        colors['Arsenal'] = graphics.Color(239, 1, 7)
+        colors['Aston Villa'] = graphics.Color(149, 191, 229)
+        colors['Bournemouth'] = graphics.Color(218, 41, 28)
+        colors['Brentford'] = graphics.Color(227, 6, 19)
+        colors['Brighton'] = graphics.Color(0, 87, 184)
+        colors['Chelsea'] = graphics.Color(3, 70, 148)
+        colors['Crystal Palace'] = graphics.Color(27, 69, 143)
+        colors['Everton'] = graphics.Color(39, 68, 136)
+        colors['Fulham'] = graphics.Color(255, 255, 255)
+        colors['Leeds'] = graphics.Color(255, 205, 0)
+        colors['Leicester'] = graphics.Color(0, 83, 160)
+        colors['Liverpool'] = graphics.Color(200, 16, 46)
+        colors['Manchester City'] = graphics.Color(108, 171, 221)
+        colors['Manchester United'] = graphics.Color(218, 41, 28)
+        colors['Newcastle'] = graphics.Color(255, 255, 255)
+        colors['Nottingham Forest'] = graphics.Color(229, 50, 51)
+        colors['Southampton'] = graphics.Color(215, 25, 32)
+        colors['Tottenham'] = graphics.Color(255, 255, 255)
+        colors['West Ham'] = graphics.Color(122, 38, 58)
+        colors['Wolves'] = graphics.Color(253, 185, 19)
+
+        return colors
+
 
 if __name__=='__main__':
+
     while True:
-        Standings().renderStandings()
+        Standings().renderEPLStandings()
