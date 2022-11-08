b1fdaab1564078940cadff6a6785575734e1d19a
jchambers26 <chambersjacobj@gmail.com>
2022-11-07 22:31:13 -0700

    Customize JSON output to contain relevant info

--- PremierLeagueStandings.py
+++ PremierLeagueStandings.py
@@ -14,10 +14,25 @@ class PremierLeagueStandings():
             'x-rapidapi-key': api_key
         }
 
+        standings = {}
+
         request = requests.get("http://v3.football.api-sports.io/standings?league=39&season=2022", headers=headers).json()
 
+        request = request['response'][0]['league']['standings'][0]
+
+        for team in request:
+            standings[team['rank']] = {
+                'team' : team['team']['name'],
+                'points' : team['points'],
+                'won' : team['all']['win'],
+                'draw' : team['all']['draw'],
+                'lost' : team['all']['lose'],
+                'gf' : team['all']['goals']['for'],
+                'ga' : team['all']['goals']['against']
+            }
+
         with open(self.path, 'w') as file:
-            json.dump(request, file)
+            json.dump(standings, file)
 
 if __name__ == '__main__':
     PremierLeagueStandings().getStandings()
