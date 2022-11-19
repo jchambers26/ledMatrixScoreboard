6376d8ff24c7f0687918c2c2d9cb7211faaa9b96
jchambers26 <chambersjacobj@gmail.com>
2022-11-18 23:49:35 -0700

    runner file to support different sports/leagues

---
+++ runner.py
@@ -0,0 +1,9 @@
+from premierLeagueRender import PremierLeagueRender
+from worldCupRender import WorldCupRender
+
+
+if __name__ == '__main__':
+    while True:
+        #PremierLeagueRender().renderPremierLeagueStandings()
+        #PremierLeagueRender().renderPremierLeagueGames()
+        WorldCupRender().renderWorldCupMatches()
